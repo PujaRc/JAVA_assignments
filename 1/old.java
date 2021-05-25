d2ec44b76e0c016215cebacfed88dda64db4c6fe
PujaRc <pujaroychowdhury@bicpu.edu.in>
2021-05-26 01:19:59 +0530

    submission1

---
+++ 1/old.java
@@ -0,0 +1,41 @@
+public class old
+{
+    public static void main(String[] args) {
+
+        //printing hello, world!
+
+        System.out.println("hello, world!");
+        System.out.println("\n");
+        //printing the sum, difference, product and quotient of two given integers
+    
+        int a = 20;
+        int b = 10;
+        System.out.println("The sum of two numbers are:" + (a+b));
+        System.out.println("The difference of two numbers are:" + (a-b));
+        System.out.println("The sum of two numbers are:" + (a*b));
+        System.out.println("The sum of two numbers are:" + (a/b));
+        System.out.println("\n");
+
+        //printing the ASCII value of tw0 hard-coded characters
+
+        char s1 = 'c';
+        char s2 = 'q';
+        int s1int = s1;
+        int s2int = s2;
+        System.out.println("ASCII value of " + s1 + " is: " + s1int);
+        System.out.println("ASCII value of " + s2 + " is: " + s2int);
+        System.out.println("\n");
+
+        //printing the four bases and their complements in a manual table
+
+        System.out.println("\t      \t DNA BASE \t \n");
+        System.out.println("---------------------------------------------");
+        System.out.println("\t CODE \t DESCRIPTION \t COMPLEMENT \n");
+        System.out.println("---------------------------------------------");
+        System.out.println("\t A \t Adenine \t T \n");
+        System.out.println("\t G \t Guanine \t C \n");
+        System.out.println("\t T \t Thymine \t A \n");
+        System.out.println("\t C \t Cytosine \t G \n");
+        
+    }
+}
---
+++ 2/Sample.java
@@ -0,0 +1,102 @@
+public class Sample
+{
+	public static void main(String[] args) {
+
+		//declarations and initializations
+
+		String DNA = "A TCG  CGTA  TGCAATGCTGCTCG  AAAA GCGCGATGC", pat = "TGC", DNA_C;
+		char[] dna = {'a', 't', 'c', 'g', 'g', 'a', 't', 'c', 'g', 'g', 'a', 't', 'c', 'g', 'g', 'a', 't', 'c', 'g', 'g'};
+		int N = DNA.length(), n = pat.length(), count = 0, count_g = 0, count_c = 0, sum, A = 0, G = 0, C = 0, T = 0, L = 20;
+		float fa, fc, ft, fg, percentage;
+
+		//pattern count in a hard-coded DNA string
+
+		for (int i = 0; i <= (N-n); i++)
+		{
+		    if (DNA.substring(i,i+n).equals(pat))
+		        count += 1;
+		}
+		
+		System.out.println(count);
+
+		System.out.println("\n");
+
+	//gap count in a hard-coded DNA string
+
+		for (int i = 0; i < N; i++){
+			if (DNA.charAt(i) == ' ')
+				count_g += 1;
+		}
+
+		System.out.println(count_g);
+
+		System.out.println("\n");
+
+		/*bug = if you insert gap between the pattern that you want to count in the previous code, then it won't be able to count the pattern. However, for real,
+		 if a gap exists in between a potential patten then that segment of sequence cannot be taken as a pattern*/
+
+		//finding complementary sequence in a hard-coded DNA string
+
+		 DNA_C =  DNA;
+
+		 DNA_C = DNA_C.replace('A', 'a');
+		 DNA_C = DNA_C.replace('T', 'A');
+		 DNA_C = DNA_C.replace('a', 'T');
+		 DNA_C = DNA_C.replace('G', 'g');
+		 DNA_C = DNA_C.replace('C', 'G');
+		 DNA_C = DNA_C.replace('g', 'C');
+
+		 System.out.println("DNA sequence: " + DNA);
+		 System.out.println("COMPLEMENTARY sequence: " + DNA_C);
+		 System.out.println("\n");
+
+		 //computing the GC percentage
+
+		count_g = 0;
+
+		for (int i = 0; i < L; i++){
+			if (dna[i] == 'a')
+				A += 1;
+			else if (dna[i] == 'c')
+				C += 1;
+			else if (dna[i] == 'g')
+				G += 1;
+			else
+				T += 1;
+		}
+
+		 fa = ((float)A/L)*100;
+		 fc = ((float)C/L)*100;
+		 ft = ((float)T/L)*100;
+		 fg = ((float)G/L)*100;
+
+		 System.out.println("The percentage of a is: " +fa);
+		 System.out.println("The percentage of c is: " +fc);
+		 System.out.println("The percentage of t is: " +ft);
+		 System.out.println("The percentage of g is: " +fg);
+		 System.out.println("\n");
+
+		 //calculating the GC percentage
+
+		for (int i = 0; i < L; i++){
+			if (dna[i] == 'c')
+				count_c += 1;
+			else if (dna[i] == 'g')
+				count_g += 1;
+			else 
+				System.out.println("");
+		}
+
+		//System.out.println("G: " +count_g);
+		//System.out.println("C: " +count_c);
+
+		sum = count_c + count_g;
+		//System.out.println("sum: " +sum);
+		percentage = ((float)sum/L)*100;
+		System.out.println("The GC percentage is: " +percentage);
+		
+		//bug = i don't have a better statement for else when the counter encounters a gap, a or t
+		//re-initialize count_g, otherwise it will store the value from the previous count and add up
+		//facing float value iaccuracy
+	}
+}
